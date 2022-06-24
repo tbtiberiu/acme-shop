1a28e28068f50c67186c53acacffb5fd986c4cc0
TibiTiberiu35 <tiberiuioan35@gmail.com>
2022-06-24 08:20:50 +0300

    Deploy to git-pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="acme-shop">
         <App />
       </BrowserRouter>
     </Provider>
